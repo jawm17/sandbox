43b0c98b383eeaf75bfb649c3f10bceed374bddb
Michael Sanderson <jawm17@gmail.com>
2020-08-31 13:05:03 -0700

    Button styling

--- sand/src/Components/Buttons.js
+++ sand/src/Components/Buttons.js
@@ -6,6 +6,7 @@ export default function Buttons() {
     return (
         <div className="buttons">
             <div className="hideSomething"></div>
+            <div className="findSomething"></div>
         </div>
     );
 }
