cc49f16f273f46a977090be4b57ae5832768371b
Michael Sanderson <jawm17@gmail.com>
2020-08-31 20:00:18 -0700

    Updates

---
+++ precache-manifest.2277b38df0778a0b9b2a54909e36553b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1e1cb599da5e55548f296456a04807",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "c064a01cdf42647f0b8a",
+    "url": "/sandbox/static/css/main.373503c9.chunk.css"
+  },
+  {
+    "revision": "c8390aa467ee95aa2414",
+    "url": "/sandbox/static/js/2.97178739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sandbox/static/js/2.97178739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c064a01cdf42647f0b8a",
+    "url": "/sandbox/static/js/main.904f3bf0.chunk.js"
+  },
+  {
+    "revision": "175c2820e642f1fb39f8",
+    "url": "/sandbox/static/js/runtime-main.d4b3c84f.js"
+  }
+]);
