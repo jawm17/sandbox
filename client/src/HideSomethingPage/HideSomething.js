cb5b34c3ab5f307becd22ae3ccca289cbd7da87a
Michael Sanderson <jawm17@gmail.com>
2020-09-11 15:39:23 -0700

    Hide something style

--- client/src/HideSomethingPage/HideSomething.js
+++ client/src/HideSomethingPage/HideSomething.js
@@ -23,15 +23,6 @@ export default function HideSomething() {
             marginLeft: 30,
             marginTop: 40,
             transition: "all 0.5s ease-in-out",
-        },
-        background: {
-            height: "100vh",
-            width: "100vw",
-            position: "fixed",
-            display: "flex",
-            justifyContent: "center",
-            top: 0,
-            left: 0
         }
     }
 
@@ -68,26 +59,24 @@ export default function HideSomething() {
 
     return (
         <div>
-            <div style={style.background} className="blackBack">
-                <Buttons page="hide" />
-                <div style={style.title}>
-                    Hide Something
-                </div>
-                <div className="inputDiv">
-                    <div className="priceTitle">
-                        $0.25 per item
-                    </div>
-                    <div className="attachment"> + add link</div>
-                    <div className="attachment"> + add image</div>
-                    <div className="attachment"> + add message</div>
-                </div>
-                <input className="itemName" onChange={(e) => inputChange(e)} value={itemName} />
-                <input className="quantity" onChange={(e) => inputChange(e)} value={quantity} />
-                <input className="link" onChange={(e) => inputChange(e)} value={link} />
-                <input className="heroImg" onChange={(e) => inputChange(e)} value={heroImg} />
-                <input className="mainImg" onChange={(e) => inputChange(e)} value={mainImg} />
-                <button onClick={() => postItem()}>submit</button>
+            <Buttons page="hide" />
+            <div style={style.title}>
+                Hide Something
             </div>
+            <div className="inputDiv">
+                <div className="priceTitle">
+                    $0.25 per item
+                </div>
+                <div className="attachment"> + add link</div>
+                <div className="attachment"> + add image</div>
+                <div className="attachment"> + add message</div>
+            </div>
+            <input className="itemName" onChange={(e) => inputChange(e)} value={itemName} />
+            <input className="quantity" onChange={(e) => inputChange(e)} value={quantity} />
+            <input className="link" onChange={(e) => inputChange(e)} value={link} />
+            <input className="heroImg" onChange={(e) => inputChange(e)} value={heroImg} />
+            <input className="mainImg" onChange={(e) => inputChange(e)} value={mainImg} />
+            <button onClick={() => postItem()}>submit</button>
         </div>
     );
 }
